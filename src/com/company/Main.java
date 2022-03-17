c7a214feba1ddfaa512a952ce7a778a9dd941967
codeefather <eduard.mojet@gmail.com>
2022-03-17 22:10:41 +0400

    Create decrypt functions (Draft version)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,33 +3,49 @@ package com.company;
 import java.util.*;
 
 public class Main {
-        public static final String[] alphabet = {"А", "Б", "В","Г","Д","Е","Ё","Ж","З","И","К","Л","М","Н",
-            "О","П","Р","С","Т","У","Ф","Х","Ц","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я"};
         public static final char[] alphabetChar = new char[]{
                 'А', 'Б', 'В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н',
                 'О','П','Р','С','Т','У','Ф','Х','Ц', 'Ч', 'Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'};
         public static final char[] punctuation = new char[]{'.',',','\"','\'',':',';','-','!','?',' '};
-        //public static HashMap<Integer, Character> punctuationMap = new HashMap<>();
         public static TreeMap<Integer, Character> punctuationMap = new TreeMap<>();
 
     public static void main(String[] args) {
 
         String textInput = "щука? сидит, на крыльце!";
+        //String textInput = "яблоко";
+
+        System.out.println("Размер алфавита");
+        System.out.println(alphabetChar.length);
+        System.out.println();
+
+        System.out.println("Индекс буквы \"Я\"");
+        System.out.println(alphabetChar.length);
+        System.out.println();
 
 
         System.out.println("Входной текст:");
         System.out.println(textInput);
-
-        System.out.println("Корректировка текста...........");
-        System.out.println("Запоминаем пунктуацию....");
+        System.out.println();
+        System.out.println("Корректировка текста...");
+        System.out.println("Запоминаем пунктуацию...");
         readPunctuation(textInput);
-        System.out.println("Удаляем пунктуацию.....");
+        System.out.println();
+        System.out.println("Удаляем пунктуацию...");
         String deletedPunctuationText = deletePunctuation(textInput);
         System.out.println(deletedPunctuationText);
+        System.out.println();
         System.out.println("Восстанавливаем пунктуацию");
         System.out.println(writePunctuation(deletedPunctuationText));
+        System.out.println();
         System.out.println("Зашифрованный текст:");
         System.out.println(encryptText(textInput, 9));
+        System.out.println();
+        System.out.println("Зашифрованный текст c пунктуацией:");
+        String cryptedText = encryptText(textInput, 9);
+        System.out.println(writePunctuation(cryptedText));
+        System.out.println();
+        System.out.println("Расшифрованный текст:");
+        System.out.println(writePunctuation(decryptText(cryptedText, 9)));
 
     }
 
@@ -43,9 +59,11 @@ public class Main {
                 if(inputChar[i] == Character.toLowerCase(alphabetChar[j])){
                     try {
                         cryptText = cryptText + alphabetChar[j + move];
+                        break;
                     } catch (ArrayIndexOutOfBoundsException e){
                         int indexOutBounds = move - (32-j);
                         cryptText = cryptText + alphabetChar[indexOutBounds - 1];
+                        break;
                     }
                 }
             }
@@ -53,15 +71,36 @@ public class Main {
         return cryptText;
     }
 
-    // + функция для поставки индекса символов пунктуации в текст
-    // функция для удаления символов пунктуации из текста
-    // + функция для записи индекса символов пунктуации
-    // + Добавить таблицу ключ - значение с:
-    //      Индекс - ключ, куда надо подставить символ
-    //      значением - символом
+    public static String decryptText(String input, int move){
+        String cryptText = "";
+        readPunctuation(input);
+        char[] inputChar = input.toCharArray();
 
+        //щука? сидит, на крыльце!
+        //ЪФЛБ? ТЙЕЙУ, ОБ ЛСЬМЭЧЁ!
+
+        // Яблоко
+        // бгнрмр
+
+        for (int i = 0; i < inputChar.length; i++) {
+            for (int j = 0; j < alphabetChar.length; j++) {
+                if(inputChar[i] == alphabetChar[j]){
+                    try {
+                        cryptText = cryptText + alphabetChar[j - move];
+                        break;
+                    } catch (ArrayIndexOutOfBoundsException e){
+                        int indexOutBounds = move - j;
+                        cryptText = cryptText + alphabetChar[alphabetChar.length - indexOutBounds];
+                        break;
+                    }
+                }
+            }
+        }
+        return cryptText;
+    }
 
     public static void readPunctuation(String input){
+
         char[] inputChar = input.toCharArray();
         for (int i = 0; i < inputChar.length; i++) {
             for (int j = 0; j < punctuation.length; j++) {
@@ -70,27 +109,31 @@ public class Main {
                 }
             }
         }
+
     }
 
 
-    public static String writePunctuation(String input){
+    public static StringBuilder writePunctuation(String input){
+
         List<Character> testChar = new ArrayList<>();
         Set<Integer> keys = punctuationMap.keySet();
 
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < input.length(); i++){
             if(Character.isAlphabetic(input.charAt(i))) {
                 testChar.add(input.charAt(i));
             }
         }
 
-
-        for (Integer key : keys)
-        {
+        for (Integer key : keys) {
             char value = punctuationMap.get(key);
             testChar.add(key, value);
         }
 
-        return testChar.toString();
+        StringBuilder text = new StringBuilder("");
+        for (char symbol : testChar) {
+            text.append(symbol);
+        }
+        return text;
     }
 
     public static String deletePunctuation(String input){
@@ -98,7 +141,6 @@ public class Main {
         char[] testArray = input.toCharArray();
         List<Character> testChar = new ArrayList<>();
         List<Integer> indexNeedToDelete = new ArrayList<>();
-        String deletedText;
 
         for (char value : testArray) {
             testChar.add(value);
@@ -115,12 +157,8 @@ public class Main {
         for (int i = 0; i < indexNeedToDelete.size(); i++) {
             testChar.remove(indexNeedToDelete.get(i) - i);
         }
-
-        deletedText = testChar.toString();
-
-        return deletedText;
+        return testChar.toString();
     }
-
 
 
 }
